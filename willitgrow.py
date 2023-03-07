a186abb670780f354908a8e9a1a0e734e7e25601
Pim <PimJit>
2023-03-07 12:46:41 -0800

    finsihed part 1 and sucessfully got hardiness zone based on zipcode

--- willitgrow.py
+++ willitgrow.py
@@ -4,10 +4,19 @@ plant_name_input = "fir"
 zipcode_input = "98105"
 
 #Part 2: get plant hardiness based on the zipcode from Frostline
+#A: get url, request, loads, and return the json data
+zipcode_request = "https://phzmapi.org/"
+zipcode_request = zipcode_request + zipcode_input + ".json"
+
+response = urllib.request.urlopen(zipcode_request)
+zipcode_response_str = response.read()
+zipcode_data = json.loads(zipcode_response_str)
+
+#B: specifically get the hardiness as a variable
+hardiness_zone = zipcode_data['zone']
+
 
 #Part 3: get plants from Perenial
-plant_dict = {"key": }
-plant_request = "https://perenual.com/api/species-list?page=1&key=sk-gneG64079b491eeca179"
 
 #Part 4: compare the plant hardiness to zipcode hardiness
 
