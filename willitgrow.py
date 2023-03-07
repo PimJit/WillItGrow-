95273910fe778ec61fcd8fd99da335a8e570c534
Skye Wright <skyekw@uw.edu>
2023-03-07 14:09:57 -0800

    worked on part 3 to return temp plant list

--- willitgrow.py
+++ willitgrow.py
@@ -17,7 +17,33 @@ hardiness_zone = zipcode_data['zone']
 
 
 #Part 3: get plants from Perenial
+#plant_dict = {"key": }
+plant_request = "https://perenual.com/api/species"
+page_num = 1
+temp_plant_list = {}
 
+def fetch_plant_data():
+    try:
+        url_request = "-list?page="+str(page_num)+"&key=sk-gneG64079b491eeca179"+"&q="+plant_name_input
+        print(plant_request+url_request)
+        return json.loads(urllib.request.urlopen(url=plant_request+url_request).read())
+    except urllib.error.URLError as e:
+        print(str(e))
+        return None
+
+def fetchPlantHardness(id):
+    url_request_id = json.loads(urllib.request.urlopen(url=plant_request+"/details/"+str(id)+"?key=sk-gneG64079b491eeca179").read())
+    temp_plant_hard_min = url_request_id["hardiness"]["min"]
+    temp_plant_hard_max = url_request_id["hardiness"]["max"]
+    return [temp_plant_hard_min, temp_plant_hard_max]
+
+def fetchPlant():
+    data = fetch_plant_data()
+    for plant in data["data"]:
+        temp_plant_list[plant["id"]] = fetchPlantHardness(plant["id"])
+
+fetchPlant()
+print(temp_plant_list)
 #Part 4: compare the plant hardiness to zipcode hardiness
 
 #Part 5: repeat until we get 5 plants that matches the hardiness zone
