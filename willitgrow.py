3144a4a74cde7c8c753312d81db7bfc449dc0f4d
Pim <PimJit>
2023-03-07 12:20:53 -0800

    descibed parts

--- willitgrow.py
+++ willitgrow.py
@@ -1,1 +1,16 @@
+import urllib.parse, urllib.request, urllib.error, json
+#Part 1: ask plant name and zipcode
+plant_name_input = "fir"
+zipcode_input = "98105"
+
+#Part 2: get plant hardiness based on the zipcode from Frostline
+
+#Part 3: get plants from Perenial
+website = "https://perenual.com/api/species-list?page=1&key=sk-gneG64079b491eeca179"
+
+#Part 4: compare the plant hardiness to zipcode hardiness
+
+#Part 5: repeat until we get 5 plants that matches the hardiness zone
+
+#Part 6: return list of plants
 
