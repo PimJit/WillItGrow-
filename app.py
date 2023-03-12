65c2f6386c4f0e485b59f1f885f78bb28cb5d4c6
Pim <PimJit>
2023-03-12 14:14:17 -0700

    updated

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from willitgrow import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/result_page", method=["GET","POST"])
+
+@app.route("/result_page", method=["GET", "POST"])
 def result_page():
     plant_name = request.form.get("plant_name")
     zipcode = request.form.get("zipcode")
-    result_list = compare_plant_to_zone(fetchPlant(plant_name), zipcode_zone(zipcode),1,plant_name)
-
-
+    result_list = compare_plant_to_zone(fetchPlant(plant_name), zipcode_zone(zipcode), 1, plant_name)
